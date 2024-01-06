9c7dba6aa3c22e9538982c740e5570f8fddb6c6c
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2024-01-06 17:17:17 +0530

    Update ViewFullDetails.jsx

--- src/components/Dashboard/Customers/ViewFullDetails.jsx
+++ src/components/Dashboard/Customers/ViewFullDetails.jsx
@@ -84,7 +84,7 @@ const ViewFullDetails = () => {
                 Pieces
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
-              `${customer}` Weight
+               {customer} Weight
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
                Mohan Weight
